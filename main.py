aef887e8289cbf687dfd37490949a9ece553e78a
TharmsyXD <133595791+TharmsyXD@users.noreply.github.com>
2023-05-28 02:10:20 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+
+def check_github(username):
+    url = f"https://api.github.com/users/{username}"
+ response = requests.get(url)
+    if response.status_code == 200:
+        print(f"{username} is a valid GitHub username!")
+    else:
+        print(f"{username} is not a valid GitHub username.")
+
+    username = input("Enter a GitHub username to check: ")
+    check_github(username)
